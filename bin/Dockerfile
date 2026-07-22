a8d482770cae6d0068d20dce0243a7702ebece47
deepakarjun <deepakarjun0@gmail.com>
2026-07-22 18:44:49 +0000

    added UST k8s

---
+++ bin/Dockerfile
@@ -0,0 +1,26 @@
+# ----------------------
+# Stage 1: Build
+# ----------------------
+FROM maven:3.9.9-eclipse-temurin-17 AS build
+
+WORKDIR /app
+COPY pom.xml .
+COPY src ./src
+
+RUN mvn clean package -DskipTests
+
+# ----------------------
+# Stage 2: Runtime
+# ----------------------
+FROM eclipse-temurin:17-jre-alpine
+
+RUN apk update && apk upgrade --no-cache
+
+RUN addgroup -S appgroup && adduser -S appuser -G appgroup
+WORKDIR /app
+
+COPY --from=build /app/target/*.jar app.jar
+
+USER appuser
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","/app/app.jar"]
---
+++ bin/kubectl.sh
@@ -0,0 +1,22 @@
+export version=1.00
+echo $version
+
+export region=us-central1
+echo $region
+
+export project_id=ust-gcp-costco-replatform-poc
+echo $project_id
+
+export ar_repo_nm=data-track-foundation
+echo $ar_repo_nm
+
+export image_nm=integration-framework-be
+echo $image_nm
+
+docker images
+docker build -t $image_nm:$version .
+docker tag $image_nm:$version $region-docker.pkg.dev/$project_id/$ar_repo_nm/$image_nm:$version
+docker rmi $image_nm:$version
+docker push $region-docker.pkg.dev/$project_id/$ar_repo_nm/$image_nm:$version
+docker rmi $region-docker.pkg.dev/$project_id/$ar_repo_nm/$image_nm:$version
+docker images
